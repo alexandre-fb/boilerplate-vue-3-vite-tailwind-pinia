1926530998b31b4763db1aab1e08f047a834b421
alexandrefb <alexandrefb.86@gmail.com>
2023-09-17 09:25:56 -0300

    Add rota exemplo

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const Example = () => import('../views/ExampleView/index.vue')
 const Home = () => import('../views/Home/index.vue')
 
 export const routes = [
     {
+        path: '/exemplo',
+        name: 'example',
+        component: Example,
+    },
+    {
         path: '/',
-        name: 'Home',
+        name: 'home',
         component: Home,
+        redirect: '/exemplo'
     }
 ]
 
