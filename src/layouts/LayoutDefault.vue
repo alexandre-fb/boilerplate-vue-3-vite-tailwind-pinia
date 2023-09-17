0d6b1ec73134d9e55ec52a271d0d324bb5ef42ab
alexandrefb <alexandrefb.86@gmail.com>
2023-09-17 19:33:25 -0300

    create file

---
+++ src/layouts/LayoutDefault.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        Header
+    </header>
+    <main>
+        <slot />
+    </main>
+    <footer>
+        Footer
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'LayoutDefault',
+    setup() {
+
+    }
+
+}
+</script>
+
+<style></style>
